72c87a6b6f76d0eb6be32fb9f9a1f3a54bd690d9
swichers-xx <seanwichers@gmail.com>
2023-06-29 19:32:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ def ping_server(server):
 
 # Check the status of all services with "voxco" in the name on a specific server
 def check_voxco_services(server):
-    cmd = f'powershell "Get-Service -ComputerName {server} *voxco* | Where-Object {{$_.Status -eq \'Running\'}} | Format-List -Property Name, Status"'
+    cmd = f'powershell "Get-Service -ComputerName {server} *voxco* | Format-List -Property Name, Status"'
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     output = process.communicate()[0].decode('utf-8').splitlines()
 
-    running_services = [line.split(':')[1].strip() for line in output if 'Name' in line]
+    running_services = [line.split(':')[1].strip() for line in output if 'Name' in line and 'Running' in line]
+    stopped_services = [line.split(':')[1].strip() for line in output if 'Name' in line and 'Stopped' in line]
 
-    return running_services
+    return running_services, stopped_services
 
 # List of servers
 servers = ["VXSQL", "VXDIRSRV", "VXTCPA", "VXDLR1", "VXDIAL", "VXCATI1", "VXCATI2", "VXREPORT", "VXOADMIN", "VXONLINE"]
@@ -22,6 +23,9 @@ servers = ["VXSQL", "VXDIRSRV", "VXTCPA", "VXDLR1", "VXDIAL", "VXCATI1", "VXCATI
 for server in servers:
     if ping_server(server):
         print(f'Server {server} is up.')
-        print('Running Voxco services:', check_voxco_services(server))
+        running_services, stopped_services = check_voxco_services(server)
+        print('Running Voxco services:', running_services)
+        if stopped_services:
+            print('ALERT: The following Voxco services are not running:', stopped_services)
     else:
         print(f'Server {server} is down.')
