b059f2fc19c567b4fd95fa9c4e3679d9af38499d
swichers-xx <seanwichers@gmail.com>
2023-06-29 19:22:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ def ping_server(server):
     response = subprocess.run(['ping', '-n', '1', server], stdout=subprocess.PIPE)
     return response.returncode == 0
 
-# Check the status of a service
-def check_service(service):
-    cmd = f'sc query "{service}" | find "RUNNING"'
+# Check the status of all services with "voxco" in the name
+def check_voxco_services():
+    cmd = 'sc query state= all | findstr /i "voxco SERVICE_NAME:"'
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    return "RUNNING" in output.decode('utf-8')
+    services = process.communicate()[0].decode('utf-8').splitlines()
+    services = [s.split(':')[1].strip() for s in services]
+
+    running_services = []
+    for service in services:
+        cmd = f'sc query "{service}" | find "RUNNING"'
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        if "RUNNING" in output.decode('utf-8'):
+            running_services.append(service)
+
+    return running_services
 
 # Test the functions
 print(ping_server('localhost'))
-print(check_service('W32Time'))  # W32Time is a common Windows service
+print(check_voxco_services())
